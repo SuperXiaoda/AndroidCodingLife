294fde283bf71e5d7ea093d6f489def8b7ef0cc3
lianghd <1401432577@qq.com>
2018-07-20 10:45:07 +0800

    Initial commit

---
+++ app/src/main/java/cn/com/brother/studio/App.java
@@ -0,0 +1,9 @@
+package cn.com.brother.studio;
+
+/**
+ * Description: Application
+ * author: LiangHD
+ * Date: 2018/7/20
+ */
+public class App {
+}
---
+++ app/src/main/java/cn/com/brother/studio/activity/MainActivity.java
@@ -0,0 +1,15 @@
+package cn.com.brother.studio.activity;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import cn.com.brother.studio.R;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
